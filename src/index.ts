92d3c8d8cadbeb33160937856155b6a23db99d79
Andre Trump <anditru@web.de>
2023-12-24 13:02:05 +0100

    Extend exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { flexPlugin } from "./flex";
 import { createProjectionState, projectionState } from "./state";
 import { transactionFilter } from "./filter";
 import { completions } from "./completions";
-import type { ProjectionPluginConfig } from "../types";
+import type { ProjectionPluginConfig } from "./types";
 import { style } from "./style";
 
 export type { ProjectionPluginConfig };
@@ -15,3 +15,18 @@ export const projectionPlugin = (config: ProjectionPluginConfig) => [
 ];
 
 export { completions };
+
+export type { CursorPositionHandler, FocusGroup } from "./focus";
+export { simpleProjection } from "./simpleProjection";
+export {
+  span,
+  staticWidget,
+  stringLiteralValue,
+  stringLiteralValueChange,
+  validateFromList
+} from "./shared";
+export  { svelteProjection } from "./svelteProjection";
+export type { Projection } from "./types";
+
+export { default as TextInput } from "./TextInput.svelte";
+export { default as Keyword } from "./Keyword.svelte";
